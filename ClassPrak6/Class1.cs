9fe9beff2a5f45a5b8c98f2c8e60743df0aefff5
DenisS <semenovdenis3201@gmail.com>
2023-05-30 22:19:24 +0300

    Добавьте файлы проекта.

---
+++ ClassPrak6/Class1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPrak6
+{
+    public class Class1
+    {
+        private int[,] matrix;
+
+        public Class1(int[,] matrix)
+        {
+            this.matrix = matrix;
+        }
+
+        public int Zadanie1()
+        {
+            int maxElement = int.MinValue;
+
+            // Заштрихованная область матрицы 8x8
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i >= j && i + j >= 7)
+                    {
+                        int element = matrix[i, j];
+                        if (element > maxElement)
+                        {
+                            maxElement = element;
+                        }
+                    }
+                }
+            }
+            return maxElement;
+        }
+
+        public int[] Zadanie2()
+        {
+            List<int> neravnol = new List<int>();
+
+            // Заштрихованная область матрицы 8x8
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i >= j && i + j >= 7 && matrix[i, j] != 0)
+                    {
+                        neravnol.Add(matrix[i, j]);
+                    }
+                }
+            }
+            return neravnol.ToArray();
+        }
+
+        public int[] Zadanie3()
+        {
+            int[] countOfTwosArray = new int[8];
+
+            // Заштрихованная область матрицы 8x8
+            for (int j = 0; j < 8; j++)
+            {
+                int ravn2 = 0;
+                for (int i = 0; i < 8; i++)
+                {
+                    if (i >= j && i + j >= 7 && matrix[i, j] == 2)
+                    {
+                        ravn2++;
+                    }
+                }
+                countOfTwosArray[j] = ravn2;
+            }
+            return countOfTwosArray;
+        }
+
+        public int Zadanie4()
+        {
+            int sum = 0;
+
+            // Правая нижняя четверть матрицы 8x8
+            for (int i = 4; i < 8; i++)
+            {
+                for (int j = 4; j < 8; j++)
+                {
+                    if (matrix[i, j] > 2)
+                    {
+                        sum += matrix[i, j];
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using ClassPrak6;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]  
+            public void Zadanie1TestMehod1()
+            {
+            int[,] matrix = new int[8, 8] {
+            { 1, 2, 3, 4, 5, 6, 7, 8 },
+            { 9, 10, 11, 12, 13, 14, 15, 16 },
+            { 17, 18, 19, 20, 21, 22, 23, 24 },
+            { 25, 26, 27, 28, 29, 30, 31, 32 },
+            { 33, 34, 35, 36, 37, 38, 39, 40 },
+            { 41, 42, 43, 44, 45, 46, 47, 48 },
+            { 49, 50, 51, 52, 53, 54, 55, 56 },
+            { 57, 58, 59, 60, 61, 62, 63, 64 }  };
+            Class1 class1Obj = new Class1(matrix);
+            int maxElement = class1Obj.Zadanie1();
+            Assert.AreEqual(64, maxElement);
+        }
+        [TestMethod]
+        public void Zadanie1TestMehod2()
+        {
+            int[,] matrix = new int[8, 8]  {       
+            { -1, -2, -3, -4, -5, 6, -7, -8 },
+            { -9, -10, -11, -12, -13, -14, -15, -16 },
+            { -17, -18, -19, -20, -21, -22, -23, -24 },
+            { -25, -26, -27, -28, -29, -30, -31, -32 },
+            { -33, -34, -35, 7, -37, -38, 39, -40 },
+            { -41, -42, -43, -44, -45, -46, -47, -48 },
+            { -49, -50, 51, -52, -53, -54, -55, -56 },
+            { -57, -58, -59, -60, -61, -62, -63, -64 } };
+            Class1 class1Obj = new Class1(matrix);
+            int maxElement = class1Obj.Zadanie1();
+            Assert.AreEqual(51, maxElement);
+        }
+
+
+        [TestMethod]
+        public void Zadanie2TestMehod1()
+        {
+            int[,] matrix = new int[8, 8];
+            Class1 obj = new Class1(matrix);
+
+            int[] expected = new int[0];
+            int[] actual = obj.Zadanie2();
+
+            Assert.AreEqual(expected.Length, actual.Length);
+        }
+        [TestMethod]
+        public void Zadanie2TestMehod3()
+        {
+            int[,] matrix = new int[8, 8];
+            Class1 obj = new Class1(matrix);
+
+            int[] expected = new int[0];
+            int[] actual = obj.Zadanie2();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
